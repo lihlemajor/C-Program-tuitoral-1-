7b38381fb4600b39c15fb8836aa80d3620d64605
Thembelihle Majoro <ict21-016@thuto.bac.ac.bw>
2022-09-03 14:26:47 +0200

    My First C# program

---
+++ Project_A/Program.cs
@@ -0,0 +1,11 @@
+﻿using Project_A;
+
+Student student = new Student();
+student.Id = 3456789;
+student.Firstname = "Lihle";
+student.Lastname = "Majoro";
+student.ContactNumber = 76965878;
+student.Address = "Gaborone, kopong";
+
+Console.WriteLine(student.ToString());
+Console.ReadLine();
---
+++ Project_A/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_A
+{
+    public class Student
+    {
+        //Declaration of Attributes/Properties
+        
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public int ContactNumber { get; set; }
+
+        public string toString()
+        {
+            return Id + " " + Firstname + " " + Lastname + " " + Address + " " + ContactNumber;
+        }
+    }
+}
